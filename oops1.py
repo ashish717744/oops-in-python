8e71314aff9949f6983126974e1c8642f6e96b53
ashish717744 <45986370+ashish717744@users.noreply.github.com>
2019-04-09 15:24:01 +0530

    Add files via upload

---
+++ oops1.py
@@ -0,0 +1,15 @@
+class Ashish:
+    umrao =5
+    def __init__ (self,car,age,name):
+        self.car =car
+        self.age =age
+        self.name =name
+
+bitto =Ashish('maruti',34,'bitoto')
+bitto.umrao =4
+print(bitto.car)
+print(bitto.age)
+print(bitto.name)
+print(bitto)
+print(bitto.umrao)
+print(Ashish.umrao)
